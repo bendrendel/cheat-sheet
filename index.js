3f1cdbfb5d210c193500ec861a5234469f1e1fce
bendrendel <bfdrendel@gmail.com>
2020-04-10 12:10:46 -0600

    add some more shit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const chuckJokeHost = 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com';
 const chuckJokeKey = '7225b26f44mshf669f1e9629b0b7p14daa7jsn6c89ef015e93';
 
 function getChuckJoke () {
-    
+
     const xhr = new XMLHttpRequest();
 
     xhr.withCredentials = true;
@@ -25,7 +25,7 @@ function getChuckJoke () {
 }
 
 function renderResponse (response) {
-    
+
     chuckJokeEl.innerHTML = response.value;
 }
 
