d5da585984437fcbacc09999a89fbbc937490fc4
bendrendel <bfdrendel@gmail.com>
2021-06-18 17:04:17 -0600

    Add info on Fetch

--- js/fetchapi.js
+++ js/fetchapi.js
@@ -3,13 +3,22 @@
 fetch() GET/POST Request Boilerplate
 
 Request is made and sent to api-to-call.com
-If a response comes back, then regardless of http status, the promise is settled as fulfilled
-Thus, in the success handler you'll want to check whether the http status is an error status or not,
+If a response comes back, then regardless of http status, the promise is settled as fulfilled with a 
+response object as the resolved value.  Response objects have useful properties like .status to get the
+http status code, and .ok which returns true if the status is between 200-299.
+
+You'll want to use .json() on the resopnse object to extract the
+json body content from the http response object.  The body mixin provides other methods to extract other 
+types of content from the body besides json, which can include ArrayBuffers, Blobs, etc. and can be extracted
+with methods like .blob() or .arrayBuffer().
+
+In the success handler you'll want to check whether the http status is an error status or not,
 Here, if the http status of the response is not ok, then the success handler throws an Error, which returns a rejected promise with the error message as at the reject reason.
 If the server could not be reached at all (e.g. server is down or heavy traffic), then the promise is settled as rejected, and the failure handler runs ("networkError")
+
 Alternatively, you may not include the network error handler, and instead put a .catch() at the end of all
 this that would catch the network error instead, as well as any http errors, or any errors that come with handling the json response.
-Note response.json() returns a promise that fulfills to the json of the initial response object.
+
 
 fetch(https://api-to-call.com/endpoint, {settings object}).then(response => {
     if (response.ok) {
@@ -22,10 +31,20 @@ fetch(https://api-to-call.com/endpoint, {settings object}).then(response => {
     return jsonResponse;
 })
 
-where {settings object} is optional depending on circumstances
-e.g. a post request needs the following to make it a POST request with data to send
-{method: 'POST', body: JSON.stringify({id: '200'})}
 
+The {settings object} is optional depending on circumstances
+e.g. a post request needs the following to make it a POST request with data to send
+{method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({id: '200'})}
+The settings object can also specify things like
+mode: cors / no-cors / same-origin / etc
+cache: no-cache / reload / force-cache / etc
+credentials: same-origin / include / omit / etc
+
+It can be useful to create a request object with new Request(), which takes the exact same 
+two parameters as fetch, i.e. a url and a settings object.  Then you can pass the request object
+directly into the fetch call.  This is handy if you need to use the same request multiple times, and
+provides easy ways to change settings object.  There is also a new Header() class to make headers that
+can be passed in.
 */
 
 //example fetch get request
